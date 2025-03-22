b7839a1ab24bf84ca11fe77b3f92d0788788b5b9
nn7773 <77133866+nn7773@users.noreply.github.com>
2025-03-21 21:17:30 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,27 @@
+Config = {}
+
+Config.OX = false -- if set to true, then this disables /storeitem, /takeitem, and /houseinventory in the chat
+
+
+-- Customizable command names
+Config.Commands = {
+    createHouse = "createhouse",
+    buyHouse = "buyhouse",
+    sellHouse = "sellhouse",
+    enterHouse = "enterhouse",
+    exitHouse = "exithouse",
+    lockHouse = "lockhouse",
+    unlockHouse = "unlockhouse",
+    storeItems = "storeitem",
+    takeItems = "takeitem",
+    houseInventory = 'pinv',  
+    showPlayerInventory = "myinv",
+    getHouses = "gethouses",
+    deleteHouse = "deletehouse",
+    invite = "invite",
+    decorateHouse = "decoratehouse",
+    listHouses = "listhouses",
+    acceptInvite = "paccept"
+}
+
+
